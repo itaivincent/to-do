2efca9c07a1e2d9d0ef3ade9b9b05b58a25ec86b
itaivincent <itaivicent321@gmail.com>
2019-09-24 08:34:56 +0200

    24 sept 2019 8:30

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -21,7 +21,7 @@
                 </div>
                 <div class="form-group col-md-3">
                   <label class="control-label">Date you made this to-do</label>
-                  <input class="form-control" name="created_at" type="text" value="{{$task->created_at}}" readonly>
+                  <input class="form-control" name="created_at" type="text" value="{{$task->deadline}}" readonly>
                 </div> 
                 <div class="form-group col-md-3">
                   <label class="control-label">Time left</label>                
@@ -38,3 +38,33 @@
         </div>
 </div>
 @endsection
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("{{$task->created_at->format('M d Y')}}").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
